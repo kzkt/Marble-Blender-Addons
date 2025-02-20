2f64e7a1d7606fd75abb38210660b46980351fe5
kzkt <kzkt@foxmail.com>
2025-02-21 00:01:07 +0800

    将hide_viewport和hide_render方法改成exclude from view layer

--- MarbleQuickMenu/scripts/OneClickBackup.py
+++ MarbleQuickMenu/scripts/OneClickBackup.py
@@ -46,8 +46,8 @@ class MQM_OT_backup_selected(bpy.types.Operator):
             context.scene.collection.children.link(backup_collection)
 
         # 隐藏集合
-        backup_collection.hide_viewport = True
-        backup_collection.hide_render = True
+        # backup_collection.hide_viewport = True
+        # backup_collection.hide_render = True
 
         # 复制对象并添加到集合
         for obj in selected_objects:
@@ -57,8 +57,17 @@ class MQM_OT_backup_selected(bpy.types.Operator):
             new_obj.name = f"{obj.name}_BAK_{current_time}"
 
             # 链接到场景并移动到备份集合
-            context.collection.objects.link(new_obj)
             backup_collection.objects.link(new_obj)
-            context.collection.objects.unlink(new_obj)
+
+        def exclude_collection(view_layer: bpy.types.ViewLayer, collecion: bpy.types.Collection):
+            for layer_collection in view_layer.layer_collection.children:
+                if layer_collection.name == collecion.name:
+                    layer_collection.exclude = True
+                    return
+                
+        current_viewlayer = bpy.context.view_layer
+
+        exclude_collection(current_viewlayer,backup_collection)
+        self.report({'INFO'},f'Successfully Backup to {backup_collection.name}')
 
         return {'FINISHED'}
